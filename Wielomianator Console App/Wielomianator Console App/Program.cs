5e2f435cb1e9577f2946baeb0ebf5521e2935600
Matt <12matQ@gmail.com>
2018-04-19 11:54:09 +0200

    update

--- Wielomianator Console App/Wielomianator Console App/Program.cs
+++ Wielomianator Console App/Wielomianator Console App/Program.cs
@@ -8,17 +8,80 @@ namespace Wielomianator_Console_App
 {
     class Number
     {
-        double thirdPowerNumber;
-        double secondPowerNumber;
-        double firstPowerNumber;
-        double lastNumber;
+        int thirdPowerNumber;
+        int secondPowerNumber;
+        int firstPowerNumber;
+        int lastNumber;
+        List<int> dzielniki;
 
-        public Number(double tPN, double sPN, double fPN, double ln)
+        public Number(int tPN, int sPN, int fPN, int ln)
         {
             thirdPowerNumber = tPN;
             secondPowerNumber = sPN;
             firstPowerNumber = fPN;
             lastNumber = ln;
+        }
+
+        public void ZnajdzDzielniki()
+        {
+            dzielniki = new List<int>();
+            if (lastNumber >= 0)
+            {
+                for (int i = -Math.Abs(lastNumber); i <= lastNumber; i++)
+                {
+                    if (i != 0)
+                    {
+                        if (lastNumber % i == 0)
+                        {
+                            dzielniki.Add(i);
+                            Console.WriteLine("Znaleziono dzielnik: " + i);
+                        }
+                    }
+                }
+            }
+
+            if (lastNumber < 0)
+            {
+                for (int i = lastNumber; i >= lastNumber; i++)
+                {
+                    if (i != 0)
+                    {
+                        if (lastNumber % i == 0)
+                        {
+                            dzielniki.Add(i);
+                            Console.WriteLine("Znaleziono dzielnik: " + i);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PorownajWynikDoZera()
+        {
+            int x;
+
+            foreach (int dzielnik in dzielniki)
+            {
+                Console.WriteLine("Sprawdzam Dzielnik: " + dzielnik);
+                x = dzielnik;
+
+                if(thirdPowerNumber*x*x*x + secondPowerNumber*x*x + firstPowerNumber*x + lastNumber == 0)
+                {
+                    Console.WriteLine("Wynik: " + dzielnik);
+                    Podziel(x, dzielnik);
+                    break;
+                }
+            }
+        }
+
+        public void Podziel(int _x, int _dzielnik)
+        {
+            int x = _x;
+            int dzielnik = _dzielnik;
+            float wynik;
+
+            wynik = (thirdPowerNumber * x * x * x + secondPowerNumber * x * x + firstPowerNumber * x + lastNumber) / (x - (dzielnik));
+            Console.WriteLine("Wynik dzielenia: " + wynik);
         }
     }
 
@@ -26,21 +89,25 @@ namespace Wielomianator_Console_App
     {
         static void Main(string[] args)
         {
-            double tPN, sPN, fPN, ln;
+            int tPN, sPN, fPN, ln;
             Console.WriteLine("Wprowadź liczbę o trzeciej potędze:");
-            tPN = Convert.ToDouble(Console.ReadLine());
+            tPN = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Wprowadź liczbę o drugiej potędze:");
-            sPN = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Wprowadź liczbę o bez potęgi:");
-            fPN = Convert.ToDouble(Console.ReadLine());
+            sPN = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wprowadź liczbę bez potęgi:");
+            fPN = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Wprowadź wyraz wolny:");
-            ln = Convert.ToDouble(Console.ReadLine());
+            ln = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(tPN.ToString()+ "x^3 + " + sPN.ToString()+ "x^2 + " + fPN.ToString()+"x + " + ln.ToString());
 
             Number polynomial = new Number(tPN, sPN, fPN, ln);
 
+            polynomial.ZnajdzDzielniki();
+            polynomial.PorownajWynikDoZera();
+
             Console.ReadLine();
         }
+
 
 
     }
